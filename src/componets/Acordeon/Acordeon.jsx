a83741c884a0f4c4d7eb1477504be08880e4836f
German Carosio <carosiogerman@gmail.com>
2024-06-07 22:36:56 -0300

    sections y divs containers para margins

--- src/componets/Acordeon/Acordeon.jsx
+++ src/componets/Acordeon/Acordeon.jsx
@@ -4,36 +4,42 @@ import styles from './Acordeon.module.css';
 const Acordeon = () => {
   return (
     <>
-        <h2 className={styles.title}>.</h2>
-        <div className={styles.accordion}>
-          <details>
-            <summary>
-              <span>Item 1</span>
+      <h2 className={styles.title}>.</h2>
+      <div className={styles.accordion}>
+        <details>
+          <summary>
+            <span>Item 1</span>
+            <div className={styles.icon}>
               <i className="fas fa-chevron-down"></i>
-            </summary>
-            <div>
-              <p>Contenido del primer item.</p>
             </div>
-          </details>
-          <details>
-            <summary>
-              <span>Item 2</span>
+          </summary>
+          <div>
+            <p>Contenido del primer item.</p>
+          </div>
+        </details>
+        <details>
+          <summary>
+            <span>Item 2</span>
+            <div className={styles.icon}>
               <i className="fas fa-chevron-down"></i>
-            </summary>
-            <div>
-              <p>Contenido del segundo item.</p>
             </div>
-          </details>
-          <details>
-            <summary>
-              <span>Item 3</span>
+          </summary>
+          <div>
+            <p>Contenido del segundo item.</p>
+          </div>
+        </details>
+        <details>
+          <summary>
+            <span>Item 3</span>
+            <div className={styles.icon}>
               <i className="fas fa-chevron-down"></i>
-            </summary>
-            <div>
-              <p>Contenido del tercer item.</p>
             </div>
-          </details>
-        </div>
+          </summary>
+          <div>
+            <p>Contenido del tercer item.</p>
+          </div>
+        </details>
+      </div>
     </>
 
   );
